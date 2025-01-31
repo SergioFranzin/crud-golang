fe3db91370998f0812aad9d58084bab174d02eb5
SergioFranzin <jr.franzin@gmail.com>
2025-01-31 13:26:01 -0300

    feat: validate request fields

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/SergioFranzin/crud-golang/src/configuration/rest_err"
+	"github.com/SergioFranzin/crud-golang/src/configuration/validation"
 	"github.com/SergioFranzin/crud-golang/src/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +12,8 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect field, error=%s", err.Error()),
-		)
-		c.JSON(restErr.Code, restErr)
+		errRest := validation.ValidateUserError(err)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 	fmt.Println(userRequest)
