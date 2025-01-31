fad624d870c61580e5165486eab4fcf942d8c8f8
SergioFranzin <jr.franzin@gmail.com>
2025-01-31 14:06:21 -0300

    feat: build logger

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,20 +1,41 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/SergioFranzin/crud-golang/src/configuration/logger"
 	"github.com/SergioFranzin/crud-golang/src/configuration/validation"
 	"github.com/SergioFranzin/crud-golang/src/model/request"
+	"github.com/SergioFranzin/crud-golang/src/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createuser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createuser"),
+		)
 		errRest := validation.ValidateUserError(err)
+
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	logger.Info("User created sucessfully",
+		zap.String("journey", "createuser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
